9e55bfc176a04cd0dabfe98298cef3cd9fee3234
GbaLog <gbalog@inbox.ru>
2016-11-21 20:24:52 +0500

    add files

---
+++ examples/test1.cpp
@@ -0,0 +1,23 @@
+/////////////////////////////////////////////////////////////////////////////////////////
+#include "../timer.hpp"
+#include <thread>
+#include <iostream>
+/////////////////////////////////////////////////////////////////////////////////////////
+int main()
+{
+    std::cout << "Testing restart():\n";
+    tools::Timer timer;
+    for( size_t it{}; it < 10; ++it )
+    {
+        std::this_thread::sleep_for(tools::milliseconds(100 * it));
+        std::cout << it << ": ";
+        if( timer.restart( tools::milliseconds(500) ) )
+        {
+            std::cout << "gone" << std::endl;
+        }
+        else
+        {
+            std::cout << "not gone" << std::endl;
+        }
+    }
+}
